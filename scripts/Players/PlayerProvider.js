68f2e0728e14599830cc390ac29a4b914fa5e26c
Austin Ring <aring97@gmail.com>
2020-07-30 16:04:10 -0500

    8088

--- scripts/Players/PlayerProvider.js
+++ scripts/Players/PlayerProvider.js
@@ -1,7 +1,7 @@
 let players=[]
 
 export const getPlayers=()=>{
-    return fetch("http://localhost:8080/players")
+    return fetch("http://localhost:8088/players")
     .then(resp=>resp.json())
     .then(parseplays=>{
         players=parseplays
