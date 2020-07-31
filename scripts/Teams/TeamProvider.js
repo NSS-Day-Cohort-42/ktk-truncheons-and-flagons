8ab2f6b3aefe549929d3022d096bdee53ccdacb2
Gavin Schriver <alexanderschriver@gmail.com>
2020-07-31 14:40:59 -0500

    finished team provider for now

--- scripts/Teams/TeamProvider.js
+++ scripts/Teams/TeamProvider.js
@@ -1,11 +1,39 @@
-let teams = []
+let teams = [];
 
-export const useTeams = () => teams.slice()
+const eventHub = document.querySelector(".container");
+
+const dispatchStateChangeEvent = () => {
+  const teamStateChangedEvent = new CustomEvent("teamStateChanged");
+
+  eventHub.dispatchEvent(teamStateChangedEvent);
+};
+
+export const saveTeam = (team) => {
+  const jsonTeam = JSON.stringify(team)
+
+  return fetch("http://localhost:8088/teams", {
+    method: "POST",
+    headers: {
+      "Content-Type" : "application/json"
+    },
+    body: jsonTeam
+  })
+    .then(getTeams)
+    .then(dispatchStateChangeEvent)
+}
+
+export const useTeams = () => teams.slice();
 
 export const getTeams = () => {
-  return fetch('http://localhost:8088/teams')
-    .then(res => res.json())
-    .then(teamData => {
-      teams = teamData
-    })
-}
+  return fetch("http://localhost:8088/teams")
+  .then((res) => res.json())
+  .then((teamData) => {
+    teams = teamData;
+  });
+  
+};
+
+
+
+
+
