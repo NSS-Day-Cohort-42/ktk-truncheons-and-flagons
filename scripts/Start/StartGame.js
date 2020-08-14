089f1a9451a24882a57dd71f0076d738d3fdce08
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-14 13:58:14 -0500

    made a dang button

---
+++ scripts/Start/StartGame.js
@@ -0,0 +1,22 @@
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".container")
+
+
+const render = () => {
+    contentTarget.innerHTML =
+    `<button class="" id="startGameButton">Start Game</button>`
+}
+
+export const StartGame = () => {
+    render()
+}
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "startGameButton") {
+        const startGameButtonEvent = new CustomEvent ("startGameButtonClicked")
+
+        eventHub.dispatchEvent(startGameButtonEvent)
+        console.log(startGameButtonEvent)
+
+    }
+})
