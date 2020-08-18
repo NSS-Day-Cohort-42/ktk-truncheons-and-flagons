89d8d4a3c0c3ab8a23358c80b691151f7cb21ef5
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-18 15:35:52 -0500

    removed scorelist call from main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,8 @@ import { PlayerForm } from "./Players/PlayerForm.js";
 import { TeamForm } from "./Teams/TeamForm.js";
 import { teamSelect } from "./Teams/TeamSelect.js";
 import { StartGame } from "./Start/StartGame.js";
-import { ScoreList, testTeamArray } from "./Scores/ScoreList.js";
 
 StartGame();
 PlayerForm();
 TeamForm();
 teamSelect();
-ScoreList(testTeamArray);
