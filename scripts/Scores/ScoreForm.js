68d449397c0f73ebbdded7f43913a726945a4366
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-08-21 14:40:42 -0500

    scoreForm complete

--- scripts/Scores/ScoreForm.js
+++ scripts/Scores/ScoreForm.js
@@ -5,28 +5,30 @@ const contentTarget = document.querySelector(".view3")
 
 eventHub.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "submitScore"){
+        //store querySelected items in variable?
+            
+        const allInputNodes = document.querySelectorAll(".teamScore")
+        //all inputs in an array [{1}, {2}, {3}]
+
+        //get the teamId from node
+        //get the score from the node (.value)
+        //return an object 
+        const specificValues = allInputNodes.map(node => {
+            const teamId = node.id.split("--")[1]
+            const score = node.value
+
+            return {
+                teamId: teamId,
+                score: score 
+            }
+
+        })
+
             const scoresSubmitted = new CustomEvent("AllScoresSubmitted", {
                 details:  {
-
-                    scoresInfo: [
-                       {
-                        teamId: //how does this get value?
-                        scores:
-                       },
-                       {
-                        teamId: 
-                        scores:
-                       },
-                       {
-                        teamId: 
-                        scores:
-                       } 
-                    ]
+                    scoresInfo: specificValues
                 }
-                
-
             })
-
 
         dispatchEvent(scoresSubmitted)
     }
@@ -41,7 +43,7 @@ const render = (arrayOfTeams) => {
         ${
             arrayOfTeams.map(teamObject => {
                 return `<div id="teamName--${teamObject.id}">${teamObject.name}</div>
-                <input id="team--${teamObject.id}" type="text" placeholder="enter round score here"></input>`
+                <input id="team--${teamObject.id}" class="teamScore" type="text" placeholder="enter round score here"></input>`
             }).join("")
         }
             <button id="submitScore">Save Round Score</button>
