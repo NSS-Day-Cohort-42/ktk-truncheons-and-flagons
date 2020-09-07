df3d239797c34f7c78698abf06e0afc6892a1bae
Jacob Eckert <jweckert17@gmail.com>
2020-09-07 17:23:19 -0500

    styling for game over screen

--- scripts/GameBuilder/GameOverScreen.js
+++ scripts/GameBuilder/GameOverScreen.js
@@ -12,13 +12,15 @@ const render = teams => {
   teams.sort((currentTeam, nextTeam) => nextTeam.score - currentTeam.score)
 
   contentTarget.innerHTML = `
-    <h2 class="game-over-text">Game Over</h2>
-    <p class="winning-team">Congratulations, ${ teams[0].name }!!! You trunched and/or flagoned better than the rest!</p>
-    <p class="game-final-ranking__header">Final Scores:</p>
-    <ol class="game-final-ranking">
-      ${ teams.map(team => `<li class="game-final-ranking__team">${team.name} (Score: ${team.score})</li>`).join("") }
-    </ol>
-    <button id="backToStart">Back to Start</button>
+    <div class="game-over-container">
+      <h2 class="game-over-text">Game Over</h2>
+      <p class="winning-team">Congratulations, ${ teams[0].name }!!! You trunched and/or flagoned better than the rest!</p>
+      <p class="game-final-ranking__header">Final Scores:</p>
+      <ol class="game-final-ranking">
+        ${ teams.map(team => `<li class="game-final-ranking__team">${team.name} (Score: ${team.score})</li>`).join("") }
+      </ol>
+      <button id="backToStart">Back to Start</button>
+    </div>
   `
 }
 
